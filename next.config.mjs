2dd740474bc6dab00ab02d2c1cbcc989e1e36104
igorweege <vieiraweege@gmail.com>
2025-06-23 17:59:19 +0000

    fix(build): configure webpack for pdfjs worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    // Adiciona uma regra para lidar com o 'worker' do pdf.js
+    config.module.rules.push({
+      test: /pdf\.worker\.min\.mjs$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[name].[hash][ext]",
+      },
+    });
+
+    return config;
+  },
+};
+
 export default nextConfig;
